b7b345e4c35397c592ca1afcd2355801bcd8804c
Amy Cohen <anbcohen@gmail.com>
2018-03-12 15:51:38 -0700

    added js for SeaTac Airport and id in CSS

--- js/app.js
+++ js/app.js
@@ -26,6 +26,39 @@ var firstPike = {
     }
   }
 };
+
 firstPike.cookiesSoldPerHour();
 console.log(firstPike.arrayOfHoursAndCookies);
+
+var seaTacAirport = {
+  nameOfLocation: 'SeaTac Airport',
+  hoursOfOperation: ['6 am', '7 am', '7 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'],
+  minCustPerHour: 3,
+  maxCustPerHour: 24,
+  avgCookiesPerCust: 1.2,
+  customersPerHour: function() {
+    var customerTrafficFlow = Math.floor(Math.random() * 65) + 1;
+    return customerTrafficFlow;
+  },
+  arrayOfHoursAndCookies: [],
+  cookiesSoldPerHour: function() {
+
+    var hourlyCookieSalesContainer = document.getElementById('seatac');
+
+    for (var i = 0; i < this.hoursOfOperation.length; i ++) {
+
+      var customersAtHour = this.customersPerHour();
+      var cookiesSoldEveryHour = Math.floor(this.avgCookiesPerCust * customersAtHour);
+
+      this.arrayOfHoursAndCookies.push(this.hoursOfOperation[i] + ': ' + cookiesSoldEveryHour + ' cookies');
+
+      var cookieSales = document.createElement('li');
+      cookieSales.textContent = this.arrayOfHoursAndCookies[i];
+      hourlyCookieSalesContainer.appendChild(cookieSales);
+    }
+  }
+};
+
+seaTacAirport.cookiesSoldPerHour();
+console.log(seaTacAirport.arrayOfHoursAndCookies);
 
