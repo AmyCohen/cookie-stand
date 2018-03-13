2b8c38334fb9413771825d65bfff3cf0bf109cd8
Amy Cohen <anbcohen@gmail.com>
2018-03-13 13:40:45 -0700

    created basic constructor function outline

--- js/app.js
+++ js/app.js
@@ -1,8 +1,48 @@
 'use strict';
 
+function SalmonCookiesByLocation(nameOfLocation, minCustPerHour, maxCustPerHour, avgCookiesPerCust) {
+  this.nameOfLocation = nameOfLocation;
+  this.minCustPerHour = minCustPerHour;
+  this.maxCustPerHour = maxCustPerHour;
+  this.avgCookiesPerCust = avgCookiesPerCust;
+  this.arrayOfHoursAndCookies = [];
+  this.runningCookieDailyTotals = 0;
+}
+
+SalmonCookiesByLocation.prototype.customersPerHour = function() {
+  var customerTrafficFlow = Math.floor(Math.random() * this.maxCustPerHour) + 1;
+  return customerTrafficFlow;
+}
+
+SalmonCookiesByLocation.prototype.cookiesSoldPerHour = function() {
+  var hourlyCookieSalesContainer = document.getElementById('pike');
+  var totalCookiesContainer = document.getElementById('pike-totals');
+
+  for (var i = 0; i < this.hoursOfOperation.length; i++) {
+
+    var customersAtHour = this.customersPerHour();
+    var cookiesSoldEveryHour = Math.floor(customersAtHour * this.avgCookiesPerCust);
+
+    this.arrayOfHoursAndCookies.push(this.hoursOfOperation[i] + ': ' + cookiesSoldEveryHour + ' cookies');
+
+    this.runningCookieDailyTotals += cookiesSoldEveryHour;
+    console.log('Pike: ' + this.runningCookieDailyTotals);
+
+    var cookieSales = document.createElement('li');
+    cookieSales.textContent = this.arrayOfHoursAndCookies[i];
+    hourlyCookieSalesContainer.appendChild(cookieSales);
+
+  }
+  var dailyTotals = document.createElement('li');
+  dailyTotals.textContent = 'Total: ' + this.runningCookieDailyTotals;
+  totalCookiesContainer.appendChild(dailyTotals);
+}
+
+hoursOfOperation: ['6 am', '7 am', '7 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'],
+
+
 var firstPike = {
   nameOfLocation: '1st and Pike',
-  hoursOfOperation: ['6 am', '7 am', '7 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'],
   minCustPerHour: 23,
   maxCustPerHour: 65,
   avgCookiesPerCust: 6.3,
