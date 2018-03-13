73e26a22fd61f51087a5134689293abd43b31175
Amy Cohen <anbcohen@gmail.com>
2018-03-13 14:02:16 -0700

    Got the new instance of Seattle Center to work correctly

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ SalmonCookiesByLocation.prototype.cookiesSoldPerHour = function() {
     this.arrayOfHoursAndCookies.push(hoursOfOperation[i] + ': ' + cookiesSoldEveryHour + ' cookies');
 
     this.runningCookieDailyTotals += cookiesSoldEveryHour;
-    console.log('Pike: ' + this.runningCookieDailyTotals);
+    console.log(this.nameOfLocation + ': ' + this.runningCookieDailyTotals);
 
     var cookieSales = document.createElement('li');
     cookieSales.textContent = this.arrayOfHoursAndCookies[i];
@@ -42,7 +42,7 @@ SalmonCookiesByLocation.prototype.cookiesSoldPerHour = function() {
 
 var firstPike = new SalmonCookiesByLocation('1st and Pike', 23, 65, 6.3);
 var seaTacAirport = new SalmonCookiesByLocation('SeaTac Airport', 3, 24, 1.2);
-
+var seattleCenter = new SalmonCookiesByLocation('Seattle Center', 11, 38, 3.7);
 
 
 firstPike.cookiesSoldPerHour();
@@ -50,42 +50,6 @@ console.log(firstPike.arrayOfHoursAndCookies);
 
 seaTacAirport.cookiesSoldPerHour();
 console.log(seaTacAirport.arrayOfHoursAndCookies);
-
-
-var seattleCenter = {
-  nameOfLocation: 'Seattle Center',
-  hoursOfOperation: ['6 am', '7 am', '7 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'],
-  minCustPerHour: 11,
-  maxCustPerHour: 38,
-  avgCookiesPerCust: 3.7,
-  runningCookieDailyTotals: 0,
-  customersPerHour: function () {
-    var customerTrafficFlow = Math.round(Math.random() * this.maxCustPerHour) + 1;
-    return customerTrafficFlow;
-  },
-  arrayOfHoursAndCookies: [],
-  cookiesSoldPerHour: function() {
-    var hourlyCookieSalesContainer = document.getElementById('seattle-center');
-    var totalCookiesContainer = document.getElementById('seattle-center-totals');
-
-    for (var i = 0; i < this.hoursOfOperation.length; i++) {
-      var customersAtHour = this.customersPerHour();
-      var cookiesSoldEveryHour =  Math.floor(this.avgCookiesPerCust * customersAtHour);
-
-      this.arrayOfHoursAndCookies.push(this.hoursOfOperation[i] + ': ' + cookiesSoldEveryHour + ' cookies');
-
-      this.runningCookieDailyTotals += cookiesSoldEveryHour;
-      console.log('Seattle Center: ' + this.runningCookieDailyTotals);
-
-      var cookiesSales = document.createElement('li');
-      cookiesSales.textContent = this.arrayOfHoursAndCookies[i];
-      hourlyCookieSalesContainer.appendChild(cookiesSales);
-    }
-    var dailyTotals = document.createElement('li');
-    dailyTotals.textContent = 'Total: ' + this.runningCookieDailyTotals;
-    totalCookiesContainer.appendChild(dailyTotals);
-  }
-};
 
 seattleCenter.cookiesSoldPerHour();
 console.log(seattleCenter.arrayOfHoursAndCookies);
